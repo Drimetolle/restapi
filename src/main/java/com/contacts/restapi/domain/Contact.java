734bfdbf35c5b86c25f13b23e4b00046b53c4cd1
Dmitry Visimskikh <drimetolle@gmail.com>
2019-04-03 21:21:10 +0500

    Initial commit

---
+++ src/main/java/com/contacts/restapi/domain/Contact.java
@@ -0,0 +1,9 @@
+package com.contacts.restapi.domain;
+
+public class Contact {
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String phoneNumber;
+}
